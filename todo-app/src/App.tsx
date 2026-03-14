669ada64c6862c3169776a9940e8809b066acd79
yuita-yoshihiko <soup998211@gmail.com>
2026-03-14 22:26:55 +0900

    shadcnが動くか確認

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>
       <div className="bg-red-500">ほげ</div>
+      <Button>Click me</Button>
     </>
   )
 }
